b1b34137be3d9398ccd1cd64b1337df146f7abe1
Rutro187 <49966402+Rutro187@users.noreply.github.com>
2019-08-01 08:58:04 -0500

    here

--- ToDolist/todo.js
+++ ToDolist/todo.js
@@ -1,11 +1,13 @@
-var submit = doument.getElementsByid(Submit);
-var priority = document.getElementById(Priority);
-var todo = document.getElementById(ToDo);
-var date = document.getElementById(TDDate)
-var open = document.getElementById(Open)
-var closed = document.getElementById(Closed)
+let submit = document.getElementById("Submit");
+let priority = document.getElementById("Priority");
+let todo = document.getElementById("ToDo");
+let date = document.getElementById("TDDate")
+let open = document.getElementById("Open")
+let closed = document.getElementById("Closed")
+let todoid = 0
 
-submit.addEventListener("submit", add);
+
+submit.addEventListener("click", add);
 
 
 
@@ -13,7 +15,24 @@ submit.addEventListener("submit", add);
 
 
 function add(){
-    
-
+    let opendiv = document.createElement("div");
+    let sevenSpaces='\xa0\xa0\xa0\xa0\xa0\xa0\xa0';
+    let button = document.createElement("input")
+    opendiv.innerText = `${todo.value} ${sevenSpaces} ${date.value} ${sevenSpaces} ${priority.value}`;
+    opendiv.classList.add("todoborder");
+    opendiv.id = todoid;
+    opendiv.appendChild(button);
+   button.value ="Delete";
+   button.type ="Button";
+   button.id= opendiv.id
+//    button.addEventListener("click", deleteTodo(todoid));
+// this shit is not working right now need to get the delete button to have the div id
+   open.appendChild(opendiv);
+   todoid ++;
+    console.log(todo.value,date.value,priority.value)
 }
 
+
+function deleteTodo() {
+    
+}
